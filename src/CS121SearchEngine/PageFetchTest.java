178080a04429c3f25c1123d9131bf309c4b31121
Shaun McThomas <smcthoma@uci.edu>
2016-02-18 16:41:08 -0800

    Forgot to add some file see last commit

---
+++ src/CS121SearchEngine/PageFetchTest.java
@@ -0,0 +1,28 @@
+package CS121SearchEngine;
+
+import java.sql.SQLException;
+
+public class PageFetchTest {
+
+	public static void main(String[] args) 
+	{
+		try {
+			
+			PageFetcher fecther = new PageFetcher();
+			int topId = PageFetcher.getLargestID();
+			System.out.println(topId);
+			System.out.println(fecther.getNext());
+			fecther.close();
+			fecther = new PageFetcher(topId-1,topId);
+			System.out.println(fecther.getNext());
+			fecther.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
